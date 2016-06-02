ff2e7a74b4736abaa19b963945638e14885c0c87
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2016-06-02 09:51:12 +0500

    Adding mysql support

---
+++ src/ru/dolika/mysql/Mysql.java
@@ -0,0 +1,36 @@
+package ru.dolika.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Mysql {
+	final Connection conn_id;
+	final Statement stmt;
+
+	public Mysql(String address, String user, String password, String database)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		this(address, user, password, database, "mysql", "com.mysql.jdbc.Driver");
+	}
+
+	public Mysql(String address, String user, String password, String database, String driverName,
+			String driverClassName)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		Class.forName(driverClassName).newInstance();
+
+		String connectionUrl = "jdbc:" + driverName + "://" + address + "/" + database;
+		conn_id = DriverManager.getConnection(connectionUrl, user, password);
+		stmt = conn_id.createStatement();
+	}
+
+	public ResultSet query(String query) {
+		try {
+			return stmt.executeQuery(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
