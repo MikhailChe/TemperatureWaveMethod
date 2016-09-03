57207bce9957611db40b68ab58a3e50fbd8f7684
Mikhail <mikhail.chernoskutov@gmail.com>
2016-09-03 23:35:22 +0500

    Maybe I can bind data? Java8 style :)

---
+++ src/ru/dolika/binder/Binder.java
@@ -0,0 +1,14 @@
+package ru.dolika.binder;
+
+import java.awt.Component;
+
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class Binder {
+
+	public Binder(Object bean, Component control) {
+		
+	}
+
+}
