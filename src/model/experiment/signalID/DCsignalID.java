9e9b1ed530f850565ed7421d5d26d7aa81d8c125
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2016-09-07 12:57:43 +0500

    Hop-hey DCSignalID lalaley.

--- src/model/experiment/signalID/DCsignalID.java
+++ src/model/experiment/signalID/DCsignalID.java
@@ -3,9 +3,9 @@ package model.experiment.signalID;
 import model.thermocouple.graduate.Graduate;
 
 public class DCsignalID extends SignalIdentifier {
-	private static final long serialVersionUID = 8360006961756985177L;
+	private static final long	serialVersionUID	= 8360006961756985177L;
 
-	Graduate graduate;
+	Graduate					graduate;
 
 	public DCsignalID() {
 		// graduate = GraduateFactory.forFile(new
@@ -13,33 +13,29 @@ public class DCsignalID extends SignalIdentifier {
 	}
 
 	public DCsignalID(Graduate grad) {
-		if (grad == null)
-			throw new NullPointerException();
+		if (grad == null) throw new NullPointerException();
 		graduate = grad;
 	}
 
-	double zeroTemperature = 273 + 22;
+	double			zeroTemperature	= 273 + 22;
 
 	// gain for 1100 = 682
 	// gain for 0100 = 271
-	double gain = 270; // /2.51
-	final double ADCgain = 32767.5;
-	final double ADCmaxVoltage = 10;
+	double			gain			= 270;		// /2.51
+	final double	ADCgain			= 32767.5;
+	final double	ADCmaxVoltage	= 10;
 
 	public double getVoltage(double ADCvalue) {
 		return ((ADCvalue / ADCgain) * ADCmaxVoltage) / gain;
 	}
 
 	public double getTemperature(double voltage) {
-		if (graduate == null)
-			return -1;
+		if (graduate == null) return -1;
 		return graduate.getTemperature(voltage, zeroTemperature);
 	}
 
 	public void setGain(double gain) {
-		if (gain != 0) {
-			this.gain = gain;
-		}
+		if (gain != 0) this.gain = gain;
 	}
 
 	public Graduate getGraduate() {
