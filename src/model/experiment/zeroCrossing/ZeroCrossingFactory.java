0829f60d777e505230be866761a3f7a221297dcd
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2016-09-07 12:37:08 +0500

    Изменение доступа к файлам юстировки.
    Было плохой идеей делать здесь HashMap, лучше использовать Hashtable и
    обернуть его в synchronizedMap.
    Теперь когда он синхронизован, можно не синхронизовывать функцию.
    Доавбляем кусочек java8 в виде метода computeIfAbsent и получаем
    лаконичный код.

--- src/model/experiment/zeroCrossing/ZeroCrossingFactory.java
+++ src/model/experiment/zeroCrossing/ZeroCrossingFactory.java
@@ -1,7 +1,9 @@
 package model.experiment.zeroCrossing;
 
+import static java.util.Collections.synchronizedMap;
+
 import java.io.File;
-import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 /**
@@ -12,7 +14,8 @@ import java.util.Map;
  */
 public class ZeroCrossingFactory {
 
-	private static Map<File, ZeroCrossing> cache = new HashMap<>();
+	private static Map<File, ZeroCrossing> cache = synchronizedMap(
+			new Hashtable<>());
 
 	/**
 	 * Выдаёт файл юстировки. Если файл уже открывался, то берется файл из кэша,
@@ -22,32 +25,11 @@ public class ZeroCrossingFactory {
 	 *            файл с юстировкой
 	 * @return новый объект класса юстировки или уже существующий объект из кэша
 	 */
-	public synchronized static ZeroCrossing forFile(File file) {
-		if (file == null) {
-			throw new NullPointerException();
-		}
-		if (cache != null) {
-			synchronized (cache) {
-				if (cache.containsKey(file) && cache.get(file) != null) {
-					return cache.get(file);
-				}
-				cache.remove(file);
-				if (!file.exists()) {
-					return null;
-				}
-				try {
-					System.out.println("Actually opening a file " + file);
-					ZeroCrossing crossing = new ZeroCrossing(file);
+	public static ZeroCrossing forFile(File file) {
+		if (file == null) throw new NullPointerException();
+		if (cache == null) return null;
 
-					cache.put(file, crossing);
-
-					return crossing;
-				} catch (IllegalArgumentException e) {
-					return null;
-				}
-			}
-		}
-		return null;
+		return cache.computeIfAbsent(file, ZeroCrossing::new);
 	}
 
 }
