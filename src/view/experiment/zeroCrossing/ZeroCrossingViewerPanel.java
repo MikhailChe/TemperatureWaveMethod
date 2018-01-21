9eb7b6032b8b1ca809914723fd0a335b6e80f374
Mikhail <mikhail.chernoskutov@gmail.com>
2018-01-21 18:17:34 +0500

    Успешный переход (а точнее сказать возврат) на jfreechart

--- src/view/experiment/zeroCrossing/ZeroCrossingViewerPanel.java
+++ src/view/experiment/zeroCrossing/ZeroCrossingViewerPanel.java
@@ -1,20 +1,22 @@
 package view.experiment.zeroCrossing;
 
 import java.awt.Dimension;
-import java.util.List;
+
+import javax.swing.SwingUtilities;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.DefaultXYDataset;
 
 import debug.Debug;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart.Data;
-import javafx.scene.chart.XYChart.Series;
 import model.experiment.zeroCrossing.ZeroCrossing;
 
-public class ZeroCrossingViewerPanel extends JFXPanel {
+public class ZeroCrossingViewerPanel extends ChartPanel {
 
 	/**
 	 * 
@@ -22,43 +24,43 @@ public class ZeroCrossingViewerPanel extends JFXPanel {
 	private static final long serialVersionUID = -7118964792998797256L;
 
 	private ZeroCrossing shifts;
-	private LineChart<Number, Number> chart;
 
-	private NumberAxis xAxis, yAxis;
+	final private NumberAxis xAxis, yAxis;
+
+	final private DefaultXYDataset dataset;
 
 	public ZeroCrossingViewerPanel(ZeroCrossing zc) {
-		super();
-
+		super(null);
 		Debug.println("Был вызван суперконструктор");
 		setPreferredSize(new Dimension(256, 256));
 		setMinimumSize(getPreferredSize());
 
-		xAxis = new NumberAxis(0, 30, 5);
+		// xAxis = new CategoryAx
 
-		yAxis = new NumberAxis(-360, 360, 45);
+		dataset = new DefaultXYDataset();
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(true, true);
+		JFreeChart chart = ChartFactory.createScatterPlot(null, null, null, dataset, PlotOrientation.HORIZONTAL, false,
+				false, false);
 
-		xAxis.setAutoRanging(false);
-		yAxis.setAutoRanging(false);
+		XYPlot xyPlot = chart.getXYPlot();
+		xyPlot.setRenderer(renderer);
 
-		xAxis.setPadding(new Insets(0));
-		yAxis.setPadding(new Insets(0));
+		xAxis = (NumberAxis) xyPlot.getRangeAxis();
+		yAxis = (NumberAxis) xyPlot.getDomainAxis();
 
-		Debug.println("Были созданы оси. Ничего страшного не должно быть");
-		chart = new LineChart<>(xAxis, yAxis);
+		xAxis.setAutoRange(false);
+		xAxis.setLowerBound(0);
+		xAxis.setUpperBound(30);
 
-		Series<Number, Number> series = new Series<>();
+		yAxis.setAutoRange(false);
 
-		chart.getData().add(series);
+		setChart(chart);
+		setPopupMenu(null);
 
-		chart.setLegendVisible(false);
+		this.setMouseZoomable(false, false);
+//		this.setDomainZoomable(false);
+//		this.setRangeZoomable(false);
 
-		chart.setPadding(new Insets(0));
-		Debug.println("Были добавлены series");
-		Debug.println("Создаём сцену");
-		Scene scene = new Scene(chart);
-		Debug.println("Устанавливаем сцену в панель");
-		Platform.runLater(() -> this.setScene(scene));
-		Debug.println("Сцена создана. Добавляем данные о юстировке");
 		if (zc != null)
 			setZeroCrossing(zc);
 	}
@@ -91,15 +93,26 @@ public class ZeroCrossingViewerPanel extends JFXPanel {
 			Debug.println("Новые данные нулевые");
 		}
 		if (this.shifts != null) {
-			Platform.runLater(() -> {
+			SwingUtilities.invokeLater(() -> {
 				rescale();
 				Debug.println("Запущена процедура по добавлению данных в другом потоке.");
-				List<Data<Number, Number>> list = chart.getData().get(0).getData();
-				list.clear();
-				for (double x = this.shifts.minFrequency(); x <= this.shifts.maxFrequency(); x += .1) {
-					Data<Number, Number> data = new Data<>(x, shifts.getCurrentShift(x));
-					list.add(data);
+
+				double minFreq = this.shifts.minFrequency();
+				double maxFreq = this.shifts.maxFrequency();
+
+				int numberOfDots = (int) Math.round(Math.ceil((maxFreq - minFreq) / .1));
+
+				double[][] data = new double[2][numberOfDots];
+
+				for (int i = 0; i < numberOfDots; i++) {
+					double freq = minFreq + i * .1;
+					data[1][i] = freq;
+					data[0][i] = this.shifts.getCurrentShift(freq);
+					Debug.println(String.format("freq: %f -> %f", freq, data[1][i]));
 				}
+
+				dataset.addSeries(0, data);
+
 				Debug.println("Процедура добавления данных выполнена");
 			});
 		}
