b414a48c61293054f7f17aa237dd85e93e89a751
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2018-02-07 11:03:02 +0500

    Platform больше не нужон

--- src/startHere/TWaveStart.java
+++ src/startHere/TWaveStart.java
@@ -3,14 +3,12 @@ package startHere;
 import javax.swing.SwingUtilities;
 
 import debug.Debug;
-import javafx.application.Platform;
 import view.experimentLauncher.ExpLauncher;
 
 public class TWaveStart {
 
 	public static void main(String[] args) {
 		Debug.println("public static void main(String[] args)");
-		Platform.setImplicitExit(false);
 		ExpLauncher launcher = new ExpLauncher();
 		SwingUtilities.invokeLater(() -> launcher.setVisible(true));
 	}
